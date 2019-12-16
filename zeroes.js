16d0b4892a0cd2b5fcb3b408cfaa6805d5d598ec
kaustubhb <kaustubhbawa123@gmail.com>
2019-12-16 14:49:17 +0530

    Create zeroes.js

---
+++ zeroes.js
@@ -0,0 +1,22 @@
+var zeroes=function(){
+  function zeroes(shape=null){
+      if(shape==null || shape.length==0){
+        return [];
+      }else{
+        if(shape.length==1){
+          return new Array(shape[0]).fill(0);
+        }else{
+          if(shape.length==2){
+            let mat=new Array(shape[0]);
+            for(let i=0;i<shape[1].length;i++){
+                mat[i]=new Array(dimension2).fill(0);
+            }
+            return mat;
+          }else{
+            throw new Error("INVALID SHAPE.");
+          }
+        }
+      }
+  }
+}
+module.exports = zeroes();
